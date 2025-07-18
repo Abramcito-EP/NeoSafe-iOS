efdc3328faa95136ea327025e0b5b4ba6970117b
Abraham Echavarria <23170110@uttcampus.edu.mx>
2025-07-18 10:55:22 -0600

    Initial Commit

---
+++ NeoSafe-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NeoSafe-iOS
+//
+//  Created by mac on 18/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
